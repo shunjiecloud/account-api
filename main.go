00e6b1e46ba5bdedb4c9e9ee158a81e4a393eb8d
zhoushengjie <zhou_shengjie@outlook.com>
2020-06-09 06:43:59 +0800

    日志调整

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/micro/go-micro/v2/web"
 	"github.com/shunjiecloud/account-api/modules"
 	"github.com/shunjiecloud/account-api/routers"
@@ -37,10 +35,10 @@ func main() {
 
 	//  init
 	if err := webSrv.Init(); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	if err := webSrv.Run(); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
